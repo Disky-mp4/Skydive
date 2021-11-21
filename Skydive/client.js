5025b499332c4d9747d0c66c982a033e8208101b
Disky <73770320+Disky-mp4@users.noreply.github.com>
2021-11-21 08:10:23 -0500

    added comments

--- Skydive/client.js
+++ Skydive/client.js
@@ -1,10 +1,10 @@
 RegisterCommand('skydive', () => {
-    const player = PlayerId(-1);
-    const ped = GetPlayerPed(-1);
-    const pedL = GetEntityCoords(ped);
-    const pedHeading = GetEntityHeading(ped);
-    StartPlayerTeleport(player, pedL[0], pedL[1], 1400, pedHeading, false, true, true);
-    setTimeout(() => {
-        TaskSkyDive(ped);
+    const player = PlayerId(-1); // get player
+    const ped = GetPlayerPed(-1); // get players ped
+    const pedL = GetEntityCoords(ped); // get the coordinates of the player
+    const pedHeading = GetEntityHeading(ped); // where the ped is heading (facing)
+    StartPlayerTeleport(player, pedL[0], pedL[1], 1400, pedHeading, false, true, true); // teleport the player
+    setTimeout(() => { // I've found that setting this too quickly won't work, so added a 500ms delay, seems to work fine
+        TaskSkyDive(ped); // set the player into skydive, allows you to control where you fall instead of falling aimlessly
     }, 500)
 })
